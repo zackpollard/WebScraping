5fbef74df70d10baf3e3072b1487ddbaa364d03d
Akeboshiwind <oliver-marshall@hotmail.com>
2014-11-08 01:44:44 +0000

    Nothing much added.

--- rpGet.py
+++ rpGet.py
@@ -22,32 +22,35 @@ base_url = "http://www.racingpost.com/horses2/results/home.sd?r_date="
 #^ prev day
 
 #### To be in loop ####
-url = base_url+str(url_date)
-r = requests.get(url)
-soup = BS(r.text)
-info = {}
-list_of_Locations = [re.search("^([^(]*)", x.string.encode("ascii")).group(0).strip() for x in soup.find("div", {"class": "tabBlock"}).ol.find_all("a")]
+try:
+  url = base_url+str(url_date)
+  r = requests.get(url)
+  soup = BS(r.text)
+  info = {}
+  list_of_Locations = [re.search("^([^(]*)", x.string.encode("ascii")).group(0).strip() for x in soup.find("div", {"class": "tabBlock"}).ol.find_all("a")]
 
-for table in soup.find_all("table", {"class": "resultGrid"}):
-  for td in table.find_all("td"):
-    try:
-      time = td.strong.get_text()
-      results = td.find_all("p")[1].get_text().strip().encode("ascii").split("\n")
-      fav = [] # mostly just one value
-      odds = [] # same as above
-      for result in results:
-        result = result.strip()
-        if result[0] == "1":
-          try:
-            int(result[-1])
-            fav.append("N")
-            odds.append(result.split()[-1])
-          except:
-            fav.append(result[-1])
-            if "even" in result.split()[-1].lower():
-              odds.append("Evens")
-            else:
-              odds.append(result.split()[-1][:-1])
-      print odds
-    except:
-      None
+  for table in soup.find_all("table", {"class": "resultGrid"}):
+    for td in table.find_all("td"):
+      try:
+        time = td.strong.get_text()
+        results = td.find_all("p")[1].get_text().strip().encode("ascii").split("\n")
+        fav = [] # mostly just one value
+        odds = [] # same as above
+        for result in results:
+          result = result.strip()
+          if result[0] == "1":
+            try:
+              int(result[-1])
+              fav.append("N")
+              odds.append(result.split()[-1])
+            except:
+              fav.append(result[-1])
+              if "even" in result.split()[-1].lower():
+                odds.append("Evens")
+              else:
+                odds.append(result.split()[-1][:-1])
+      except:
+        None
+
+except:
+  None
