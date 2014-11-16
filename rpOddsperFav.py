1948c2fd9dc7c88b1b898d8bb16554b2d082986e
Akeboshiwind <oliver-marshall@hotmail.com>
2014-11-16 01:23:32 +0000

    Finnished it, this one comes up as a bell curve which is quite cool. Didn't expect that.

--- rpOddsperFav.py
+++ rpOddsperFav.py
@@ -19,6 +19,37 @@ for file in files:
   for date in data:
     for location in date[2:]:
       for time_race in location[1:]:
-        for fav in time_race[1]:
-          None
-        for odds in time_race[2]:
+        for num in range(len(time_race[2])):
+          odds = time_race[2][num]
+          fav = time_race[1][num]
+
+          if (odds in x) or ((odds.lower() == "evens") and ("1/1" in x)):
+            if odds.lower() == "evens":
+              index = x.index("1/1")
+            else:
+              index = x.index(odds)
+            if fav == "F":
+              y[index] += 1
+          else:
+            if fav == "F":
+              if odds.lower() == "evens":
+                x.append("1/1")
+              else:
+                x.append(odds)
+              y.append(1)
+x, y = (list(t) for t in zip(*sorted(zip(x, y), key=lambda tup: float(tup[0].split("/")[0])/float(tup[0].split("/")[1]))))
+
+fig = plt.figure(figsize=(20, 5))
+ax = plt.plot()
+
+index = np.arange(len(x))
+x = np.array(x)
+y = np.array(y)
+
+graph = plt.plot(index, y)
+
+plt.xlabel("Odds")
+plt.ylabel("Num Favs")
+plt.xticks(rotation=70)
+plt.xticks(np.linspace(0, len(x), len(x)), x)
+plt.show()
