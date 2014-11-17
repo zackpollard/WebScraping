8c78f672d8cf9fdcfe9d790bdc42df6bf689d616
Akeboshiwind <oliver-marshall@hotmail.com>
2014-11-17 00:44:42 +0000

    Added a file to calculate the longest gap between races.

---
+++ rpLongestGap.py
@@ -0,0 +1,31 @@
+import json, os, time, datetime, calendar
+from os import listdir
+from os.path import isfile, join
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(".\\rp_jsonOrdered")
+path = os.getcwd()
+files = ["rp"+str(x)+".json" for x in range(len([name for name in os.listdir('.') if os.path.isfile(name)]))]
+
+longest_gap = 0
+longest_gap_date = ""
+
+for file in files:
+  with open(file, "r") as f:
+    data = json.loads(f.read())
+
+  for date in data:
+    num_race = 0
+    gap = -1
+    for time_race in date[1:]:
+      num_race += 1
+      gap += 1
+      if "F" in time_race[1]:
+        if longest_gap < gap:
+          longest_gap = gap
+        if gap == 26:
+          longest_gap_date = date[0]
+        if gap > 10:
+          print gap, date[0]
+        gap = -1
+print "Longest Gap:", longest_gap, "on", longest_gap_date
