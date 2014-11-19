fff43020e2d8aef554ac20fd8c19a3277e26ec86
Akeboshiwind <oliver-marshall@hotmail.com>
2014-11-19 15:39:53 +0000

    Started a file to Simulate actual betting.

---
+++ rpSimulate.py
@@ -0,0 +1,18 @@
+import json, os
+from os import listdir
+from os.path import isfile, join
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(".\\rp_json")
+path = os.getcwd()
+files = ["rp"+str(x)+".json" for x in range(len([name for name in os.listdir('.') if os.path.isfile(name)]))]
+
+for file in files:
+  with open(file, "r") as f:
+    data = json.loads(f.read())
+
+  for date in data:
+    for location in date[2:]:
+      for time_race in location[1:]:
+        for fav in time_race[1]:
+          None
