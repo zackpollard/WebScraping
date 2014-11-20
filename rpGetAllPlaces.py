fa7bda5e576a34b29560b0dc9a507285216661ca
Akeboshiwind <oliver-marshall@hotmail.com>
2014-11-20 21:54:18 +0000

    Lot's of changes yaaaay!

--- rpGetAllPlaces.py
+++ rpGetAllPlaces.py
@@ -9,14 +9,14 @@
 #
 ####
 
-import sys, json, requests, datetime, time, re
+import sys, json, requests, datetime, time, re, operator
 from bs4 import BeautifulSoup as BS
 
 base_url = "http://www.racingpost.com/horses2/results/home.sd?r_date="
 url_date = datetime.date.today() - datetime.timedelta(days = 1)
-#url_date = datetime.date(2013, 12, 12)
+url_date = datetime.date(2009, 12, 12)
 
-folder_name = "rp_json"
+folder_name = "rp_allPlaces"
 limit_locations = True
 limited_locations = []
 
@@ -27,7 +27,7 @@ count_reset = 50
 
 years_wanted = 10#number of years of data wanted
 days_wanted = years_wanted*365
-#days_wanted = 1
+days_wanted = 10
 #today - datetime.timedelta(days = 1)
 #^ prev day
 
@@ -43,8 +43,8 @@ for i in xrange(days_wanted):
 
   now = float(time.time())#start time
   url = base_url+str(url_date) #makes the url
-  r = requests.get(url) #get's the web page
-  soup = BS(r.text) #soupify!
+  main_r = requests.get(url) #get's the web page
+  soup = BS(main_r.text) #soupify!
   #gets the list of locations
   list_of_Locations = [re.search("^([^(]*)", x.string.encode("ascii")).group(0).strip() for x in soup.find("div", {"class": "tabBlock"}).ol.find_all("a")]
   #may get what appears to be the same location twice in a row, this is because I get rid of brackets.
@@ -56,43 +56,53 @@ for i in xrange(days_wanted):
   #loops over each course's (location) results
   for table in soup.find("div", {"id": "resultTag"}).find_all("table", {"class": "resultGrid"}):
     if list_of_Locations[0] in limited_locations:
-      location = [] #second layer of info
-      location.append(list_of_Locations.pop(0))
+      location = list_of_Locations.pop(0)
+      #print location
       #loops over each result (time_race)
       for td in table.find_all("td"):
         try:#some of the "td" have no info we need so they will fail
           time_race = [td.strong.get_text().encode("ascii")] #time of race
           #gets each time's results, needed because sometimes there are two winners
-          results = td.find_all("p")[1].get_text().strip().encode("ascii").split("\n")
-          fav = [] # mostly just one value
-          odds = [] # same as above
-          found_first = False
+          results_url = "http://www.racingpost.com/" + td.h4.a["href"]
+          results_r = requests.get(results_url)
+          results_soup = BS(results_r.text)
+          results = []
+
           num_races += 1
-          for result in results:
-            result = result.strip()#nasty trailing whitespace
-            try:
-              if result[0] == "1":#gets the winners
-                found_first = True
-                if result[-1].isupper():
-                  fav.append(result[-1])
-                  if result.split()[-1][0].isalpha():
-                    if ("ev" in result.split()[-1].lower()) or ("odds" in result.split()[-1].lower()):
-                      odds.append("Evens")
+          for result in results_soup.find_all("tr"):
+            if result.has_attr("data-hid"):
+              try:
+                place = int(result.find("h3").string)
+                #print result.find("span", {'class': 'black'}).text
+                odds_fav = result.find("span", {'class': 'black'}).text.split(" ")[-2]
+                if odds_fav[-1].isupper():
+                  fav = odds_fav[-1]
+                  if odds_fav[0].isalpha():
+                    if "ev" in odds_fav[-1].lower() or "odds" in odds_fav[-1].lower():
+                      odds = "Evens"
                     else:
-                      odds.append("Unknown")
+                      odds = "Unknown"
                   else:
-                    odds.append(result.split()[-1][:-1])
+                    odds = odds_fav[:-1]
                 else:
-                  fav.append("N")
-                  if result.split()[-1][0].isalpha():
-                    if ("ev" in result.split()[-1].lower()) or ("odds" in result.split()[-1].lower()):
-                      odds.append("Evens")
+                  fav = "N"
+                  if odds_fav[0].isalpha():
+                    if "ev" in odds_fav[-1].lower() or "odds" in odds_fav[-1].lower():
+                      odds = "Evens"
                     else:
-                      odds.append("Unknown")
+                      odds = "Unknown"
                   else:
-                    odds.append(result.split()[-1])
-            except:
-              None
+                    odds = odds_fav
+                results.append([place, fav, odds])
+              except:
+                None
+          results_sorted = sorted(results, key=operator.itemgetter(0))
+          #print results_sorted
+        except:
+          None
+  print num_races
+  url_date = url_date - datetime.timedelta(days = 1)
+'''
           if found_first == False:
             fav.append("N")
             odds.append("Unknown")
@@ -121,4 +131,4 @@ for i in xrange(days_wanted):
     time.sleep(time_between - delay)#so I don't overload the site(or get caught...)
 
 with open(folder_name + "\\rp"+str(count_name)+".json", "w") as f:
-  f.write(json.dumps(data))
+  f.write(json.dumps(data))'''
