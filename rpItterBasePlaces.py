0ab97b3d42071bab0f25f6509e362bcf7b55da99
Akeboshiwind <oliver-marshall@hotmail.com>
2014-11-20 23:01:09 +0000

    Added a genral file for itterating over the new file format.

---
+++ rpItterBasePlaces.py
@@ -0,0 +1,22 @@
+import json, os
+from os import listdir
+from os.path import isfile, join
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(".\\rp_allPlaces")
+path = os.getcwd()
+files = ["rp"+str(x)+".json" for x in range(len([name for name in os.listdir('.') if os.path.isfile(name)]))]
+
+for file in files:
+  with open(file, "r") as f:
+    date = json.loads(f.read())
+  print "!"
+  string_date = date[0]
+  no_of_races = date[1]
+  for time_race in date[2:]:
+    time_of_race = time_race[0]
+    location = time_race[1]
+    for result in time_race[2]:
+      place = result[0]
+      fav = result[1]
+      odds = result[2]
