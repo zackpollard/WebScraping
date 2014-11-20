7dbce5ff044aac68e8324f8d901aa7699b1413c3
Akeboshiwind <oliver-marshall@hotmail.com>
2014-11-20 23:08:03 +0000

    Now gathers information, untested on large amounts of data. Todo: Update feature. Asyncronous downloads.

--- rpGetAllPlaces.py
+++ rpGetAllPlaces.py
@@ -9,7 +9,7 @@
 #
 ####
 
-import sys, json, requests, datetime, time, re, operator
+import sys, json, requests, datetime, time, re, operator, os
 from bs4 import BeautifulSoup as BS
 
 base_url = "http://www.racingpost.com/horses2/results/home.sd?r_date="
@@ -19,11 +19,16 @@ url_date = datetime.date(2009, 12, 12)
 folder_name = "rp_allPlaces"
 limit_locations = True
 limited_locations = []
+update = False
 
 time_between = 0.5# time between requests in secinds
-count = 0
-count_name = 0
-count_reset = 50
+if update:
+  cwd = os.getcwd()
+  os.chdir(".\\" + folder_name)
+  count_name = len([name for name in os.listdir('.') if os.path.isfile(name)])
+  os.chdir(cwd)
+else:
+  count_name = 0
 
 years_wanted = 10#number of years of data wanted
 days_wanted = years_wanted*365
@@ -36,7 +41,6 @@ if limit_locations:
     for line in f.read().split("\n"):
       limited_locations.append(line)
 
-data = []
 #### To be in loop ####
 for i in xrange(days_wanted):
   print str(url_date)
@@ -51,17 +55,19 @@ for i in xrange(days_wanted):
   if not limit_locations:
     limited_locations = list_of_Locations
   date = [str(url_date)] #creates out first layer of info
+  all_times = []
   num_races = 0
 
   #loops over each course's (location) results
   for table in soup.find("div", {"id": "resultTag"}).find_all("table", {"class": "resultGrid"}):
     if list_of_Locations[0] in limited_locations:
-      location = list_of_Locations.pop(0)
+      location = list_of_Locations[0]
+      list_of_Locations.pop(0)
       #print location
       #loops over each result (time_race)
       for td in table.find_all("td"):
         try:#some of the "td" have no info we need so they will fail
-          time_race = [td.strong.get_text().encode("ascii")] #time of race
+          time_race = [td.strong.get_text().encode("ascii"), location] #time of race
           #gets each time's results, needed because sometimes there are two winners
           results_url = "http://www.racingpost.com/" + td.h4.a["href"]
           results_r = requests.get(results_url)
@@ -97,38 +103,16 @@ for i in xrange(days_wanted):
               except:
                 None
           results_sorted = sorted(results, key=operator.itemgetter(0))
-          #print results_sorted
+          time_race.append(results_sorted)
+          all_times.append(time_race)
         except:
           None
-  print num_races
-  url_date = url_date - datetime.timedelta(days = 1)
-'''
-          if found_first == False:
-            fav.append("N")
-            odds.append("Unknown")
-          time_race.append(fav)
-          time_race.append(odds)
-          location.append(time_race)
-        except:
-          None
-      date.append(location)
-    else:
-      list_of_Locations.pop(0)
-  date.insert(1, num_races)
-  data.append(date)
+  all_times = sorted(all_times, key=operator.itemgetter(0))
+  date.append(num_races)
+  date = date + all_times
+
+  with open(folder_name+"\\rp"+str(count_name)+".json", "w") as f:
+    f.write(json.dumps(date))
+    count_name += 1
 
   url_date = url_date - datetime.timedelta(days = 1)
-  if count > count_reset:
-    with open(folder_name + "\\rp"+str(count_name)+".json", "w") as f:
-      f.write(json.dumps(data))
-      count = -1
-      count_name += 1
-      data = []
-  count += 1
-
-  delay = float(time.time()) - now#how long it took
-  if delay < time_between:
-    time.sleep(time_between - delay)#so I don't overload the site(or get caught...)
-
-with open(folder_name + "\\rp"+str(count_name)+".json", "w") as f:
-  f.write(json.dumps(data))'''
